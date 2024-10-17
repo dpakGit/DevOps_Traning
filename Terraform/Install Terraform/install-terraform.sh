be2c40f4873f6b08c494c0d935be62f192b857a9
Deepak <126373117+Deepakranjanmishra@users.noreply.github.com>
2024-10-17 12:34:45 +0530

    Update install-terraform.sh

--- Terraform/Install Terraform/install-terraform.sh
+++ Terraform/Install Terraform/install-terraform.sh
@@ -2,14 +2,13 @@
 
 # Install Terraform
 echo "[TASK 1] Install Base Packages"
-apt update
-apt install
-apt install unzip -y
+#apt update
+#apt install unzip -y
 
 # Download Terraform
 echo "[TASK 2] Install Terraform Packages"
 wget https://releases.hashicorp.com/terraform/1.9.7/terraform_1.9.7_linux_amd64.zip
-unzip /terraform_1.9.7_linux_amd64.zip 
+unzip terraform_1.9.7_linux_amd64.zip
 mv terraform /usr/bin/
 chmod +x /usr/bin/terraform
 terraform --version
