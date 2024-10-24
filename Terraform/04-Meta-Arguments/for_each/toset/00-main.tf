37b2a60637c44371fa23c5d4aa4292b62deeec75
Deepak <126373117+Deepakranjanmishra@users.noreply.github.com>
2024-10-24 19:08:33 +0530

    Create 00-main.tf

---
+++ Terraform/04-Meta-Arguments/for_each/toset/00-main.tf
@@ -0,0 +1,17 @@
+# Compare 00-main.tf and main.tf examples, both gives the same result
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "user_names" {
+  description = "IAM usernames"
+  type        = set(string)
+  default     = ["Todd-2", "James-2", "Alice-2", "Dottie-2"]                             
+}
+
+resource "aws_iam_user" "example" {
+  for_each = var.user_names
+  name  = each.value
+}
+
