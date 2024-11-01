55fcfc42202552367c9453a49044a0f304c8a93d
Deepak <126373117+Deepakranjanmishra@users.noreply.github.com>
2024-11-01 12:19:53 +0530

    Create variable.tf

---
+++ Terraform/05-variables/.tfvars-files/case-2/variable.tf
@@ -0,0 +1,17 @@
+# Declare variables
+
+variable "image_id" {
+ type = string
+}
+
+
+variable "zones" { 
+ type = list(string)
+ default = ["us-east-1a", "us-east-1b"]
+}
+
+
+variable "myname" {
+ type = string
+ default = "mydefault-instances"
+}
