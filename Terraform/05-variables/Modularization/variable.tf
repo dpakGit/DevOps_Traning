a892e1ed495f8de9decc18c6c0a59e7be866cd4d
Deepak <126373117+dpakGit@users.noreply.github.com>
2024-11-27 12:21:27 +0530

    Update variable.tf

--- Terraform/05-variables/Modularization/variable.tf
+++ Terraform/05-variables/Modularization/variable.tf
@@ -1,9 +1,19 @@
+# 1. Variable: - "ZONES"
+
 variable "zones" {
   type = list(string)
   default = ["us-east-1a", "us-east-1b"]
 }
 
+# 2. Variable: - "Names"
+
 variable "myname" {
   type = string
   default = "mydefault-instances"
+}
+
+# 3. Variable: - "CIDR Range"
+
+variable cidr {
+    default = "11.11.11.11/32"
 }
