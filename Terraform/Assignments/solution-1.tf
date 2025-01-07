ad4d2f44a17d754f19fbace3e98e30cb26c2caf6
Deepak <126373117+dpakGit@users.noreply.github.com>
2025-01-07 11:50:49 +0530

    Update solution-1.tf

--- Terraform/Assignments/solution-1.tf
+++ Terraform/Assignments/solution-1.tf
@@ -1,1 +1,130 @@
+#  Code-1  - NEW - Version - 1 - Ami hardcoded
 
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "east2"
+}
+
+# N.virginia 
+
+#````````````````````````````````````````````````````````
+
+# Variable Block
+
+
+variable "zones_east-1" { #  Zone-1    :  N.Virginia               
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+
+
+# Resource Block in  US-east-1 - N.Virginia
+
+
+
+
+
+resource "aws_instance" "Backend-East-1" { # Backend Resource Block - N.Virginia
+
+  ami               = "ami-0e2c8caa4b6378d8c" # AMI : Ubuntu Server 24.04 LTS (HVM),EBS General Purpose (SSD) Volume Type
+  instance_type     = "t2.micro"
+  availability_zone = var.zones_east-1[count.index]
+  count             = 2
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name = "Backend-East-1-${count.index + 1}" # Also we can name it as "Backend_N.Virginia-${count.index + 1}" or Baknd_N.Virginia               
+    Team = "DevOps"
+  }
+}
+
+# Frontend Resource Block - N.Virginia
+
+resource "aws_instance" "Frontend-East-1" {
+  ami               = "ami-0e2c8caa4b6378d8c" # AMI : Ubuntu Server 24.04 LTS (HVM),EBS General Purpose (SSD) Volume Type
+  instance_type     = "t2.micro"
+  availability_zone = var.zones_east-1[count.index]
+
+  count = length(aws_instance.Backend-East-1)
+
+  depends_on = [aws_instance.Backend-East-1]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "Frontend-East-1-${count.index + 1}"
+    Team = "DevOps"
+  }
+}
+
+#``````````````````````````````````````````````````
+
+
+
+# Ohio
+
+
+variable "zones_east-2" {
+  default = ["us-east-2a", "us-east-2b"]
+}
+
+
+# Resource Block in  US-east-2 - Ohio
+
+
+
+
+# Backend Resource Block - Ohio
+
+resource "aws_instance" "Backend-East-2" {
+  provider = aws.east2
+
+  ami               = "ami-00eb69d236edcfaf8"
+  instance_type     = "t2.micro"
+  availability_zone = var.zones_east-2[count.index]
+
+  count = 2
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name = "Backend-East-2-${count.index + 1}" # "Backend_Ohio-${count.index + 1}"
+    Team = "DevOps"
+  }
+}
+
+# Frontend Resource Block - Ohio
+
+resource "aws_instance" "Frontend-East-2" {
+  provider = aws.east2
+
+  ami = "ami-00eb69d236edcfaf8"
+
+  instance_type     = "t2.micro"
+  availability_zone = var.zones_east-2[count.index]
+
+  count = length(aws_instance.Backend-East-2)
+
+  depends_on = [aws_instance.Backend-East-2]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "Frontend-East-2-${count.index + 1}"
+    Team = "DevOps"
+  }
+}
